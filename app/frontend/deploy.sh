4b046f968cf29eace74d0ad42b87816ac45c255e
Tuienn <tuyenboy1234@gmail.com>
2025-08-10 01:14:30 +0700

    fix deploy.sh

--- app/frontend/deploy.sh
+++ app/frontend/deploy.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-
-# Dừng tiến trình Next.js đang chạy bằng PM2
 pm2 stop nextjs-fe
-
-# Chuyển đến thư mục frontend
 cd /root/Kmasc/app/frontend || exit 1
 
-# Build lại dự án
+if [ "$1" = "reinstall" ]; then
+    npm install
+fi
+
 npm run build
-
-# Start lại Next.js bằng PM2 với tên nextjs-fe
 HOST=0.0.0.0 pm2 start npm --name nextjs-fe -- start
-
-# Lưu cấu hình PM2
 pm2 save
-
-# Copy build sang thư mục web server
 cp -r ./.next/* /var/www/frontend/
-
-# Cấp quyền cho www-data
 chown -R www-data:www-data /var/www/frontend
-
-# Reload Nginx
 systemctl reload nginx
